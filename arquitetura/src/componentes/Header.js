7929142ac149b7f2aaf19c8120729f00e4bf34d0
dougJesus <douglas.jb@outlook.com.br>
2024-01-27 14:21:21 -0200

    adicioando arquivos

---
+++ arquitetura/src/componentes/Header.js
@@ -0,0 +1,9 @@
+import styles from "../componentes/Header.module.css"
+function Header(){
+    return(
+        <div classNme= "imgs">
+           <h2>tstet</h2>
+        </div>
+    )
+}
+export default Header
